2227a31d29834166624dde69f882d64760549e70
nagoya0 <ninot@hotmail.co.jp>
2022-03-08 19:05:32 +0900

    Add project

---
+++ Assets/Scripts/InputFieldRichText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputFieldRichText : MonoBehaviour
+{
+    [SerializeField] ToggleButton _toggleButtonBold = null;
+    [SerializeField] ToggleButton _toggleButtonUnderline = null;
+    [SerializeField] ToggleButton _toggleButtonRed = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
